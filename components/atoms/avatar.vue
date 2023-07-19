a4122ef6ea7d390bb70d6c62353e2d1455bce61c
Julien Da Costa <juliendacosta27@gmail.com>
2023-07-19 12:16:22 +0200

    avatar called in header

--- components/atoms/avatar.vue
+++ components/atoms/avatar.vue
@@ -1,1 +1,14 @@
-<template></template>
+<template>
+  <Avatar />
+</template>
+
+<script>
+import Avatar from "vue-boring-avatars";
+
+export default {
+  name: "AvatarComponent",
+  components: {
+    Avatar,
+  },
+};
+</script>
--- components/organisms/header.vue
+++ components/organisms/header.vue
@@ -1,1 +1,14 @@
-<template></template>
+<template>
+  <AvatarComponent></AvatarComponent>
+</template>
+
+<script>
+import AvatarComponent from "components/atoms/avatar"
+
+export default {
+  name: "Header",
+  components: {
+    AvatarComponent,
+  }
+}
+</script>
