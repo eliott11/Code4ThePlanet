d50e4237f09cb3bd5ae442f244018d377ec06d4a
Eliott Demont <eliott.demont@edu.devinci.fr>
2023-07-19 15:01:25 +0200

    add footer & config icons lib

--- components/organisms/footer.vue
+++ components/organisms/footer.vue
@@ -1,1 +1,27 @@
-<template></template>
+<template>
+  <div class="w-full bg-zinc-300 flex justify-between">
+    <div class="m-5">
+      <h2 class="font-bold">CONTACT</h2>
+      <p>Réseau alumni du Pôle Léonard de Vinci</p>
+      <p>10 Av. Léonard de Vinci 92600 COURBEVOIE</p>
+      <div class="flex">
+        <Icon name="ph-envelope-simple" class="m-1" />
+        <p>lumni@devinci.fr</p>
+      </div>
+    </div>
+    <div class="flex flex-col m-5">
+      <div>
+        SUIVEZ-NOUS
+      </div>
+      <div>
+        <Icon name="ph-facebook-logo" />
+        <Icon name="ph-linkedin-logo" />
+        <Icon name="ph-instagram-logo" />
+      </div>
+    </div>
+    <div>
+
+    </div>
+  </div>
+
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     '@vueuse/nuxt',
+    'nuxt-icon',
   ],
   css: ['~/assets/css/main.css'],
   postcss: {
