02999809faa5101e782042c7d0cfaa205eed9194
ColinEspinas <colin.espinas@yahoo.fr>
2023-07-19 15:32:26 +0200

    feat: added section component

--- components/molecules/section.vue
+++ components/molecules/section.vue
@@ -1,1 +1,12 @@
-<template></template>
+<script setup lang="ts">
+defineProps<{
+  title: string
+}>()
+</script>
+
+<template>
+  <section class="border rounded p-4">
+    <h2 class="font-bold text-xl mb-2">{{ title }}</h2>
+    <slot></slot>
+  </section>
+</template>
